8ffc8cdff87bcfaf240f483e4caf1b11bb928e67
Walter Sosa <wasosa@yahoo.com>
2023-12-07 11:21:21 -0800

    Make the version pop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os 
+import textwrap
+import os
 
 from flask import Flask, jsonify
 from waitress import serve
@@ -8,10 +9,23 @@ app = Flask(__name__)
 
 app_version = os.environ["APP_VERSION"]
 
+
 @app.route("/", methods=["GET"])
 def get_version():
-    data = {"message": f"Hey you, this server is running {app_version} as the application version"}
-    return jsonify(data)
+    return textwrap.dedent(
+        f"""\
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta name="viewport" content="width=device-width, initial-scale=10.0" />
+            <title>Hacky App {app_version}</title>
+          </head>
+          <h1 style="text-align:center;">This server is running version:</h1>
+          <div style="text-align:center; color:red;font-size:3.0rem;">{app_version}</div>
+        </html>
+    """
+    )
+
 
 if __name__ == "__main__":
     serve(TransLogger(app, setup_console_handler=False))
